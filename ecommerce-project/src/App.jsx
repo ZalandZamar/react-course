4c15709f1c598c426e0b7b7742d8283b97ceba02
zaland <sanikhankhan749@gmail.com>
2025-07-16 13:32:20 -0700

    Genrate HTML for the cart items

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -12,8 +12,10 @@ import './App.css'
 function App() {
   const [cart, setCart] = useState([]);
   useEffect(() => {
-    axios.get('http://localhost:3000/api/cart-items').then((response) => {
-      setCart(response.data);      
+    axios.get('/api/cart-items?expand=product').then((response) => {
+      setCart(response.data);    
+      console.log(response.data);
+        
     });
   }, []);
 
