1f4caa755bb592c78aab6c1831af649d3144696a
Zaland <sanikhankhan749@gmail.com>
2025-07-18 22:12:16 -0700

    Create add to crat feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -11,18 +11,19 @@ import './App.css'
 
 function App() {
   const [cart, setCart] = useState([]);
-  useEffect(() => {
-    const fetchAppData = async () => {
-      const response = await axios.get('/api/cart-items?expand=product');
-      setCart(response.data);
-    }
 
-    fetchAppData();
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data);
+  }
+
+  useEffect(() => {
+    loadCart();
   }, []);
 
   return (
     <Routes>
-      <Route path='/' element={<HomePage cart={cart} />}></Route>
+      <Route path='/' element={<HomePage cart={cart} loadCart={loadCart} />}></Route>
       <Route path='/checkout' element={<CheckoutPage cart={cart} />}>
       </Route>
       <Route path="/orders" element={<OrdersPage cart={cart} />}></Route>
